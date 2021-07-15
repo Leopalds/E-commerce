0f3923e8d59ce6226d0bc989c030002a95c2df1a
Pedro Leopoldo <pedro.leopoldo@estudante.ufjf.br>
2021-07-15 16:45:37 -0300

    Alguns arquivos que faltaram

---
+++ e-commerce-bala/resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layout.main')
+@section('css')
+    <link rel="stylesheet" href="css/home.css">
+@endsection
+
+@section('conteudo')
+    <div class="banner">
+        <img src="{{asset('img/bebidas.jpg')}}" class="banner-img img-fluid" alt="Banner da Hulmer's">
+        <figcaption>A sua cerveja entregue por quem <span>ama</span></figcaption>
+    </div>
+    <div class="container">
+        <h1 class="text-center mt-3">Produtos que Hulmer's recomenda para você</h1>
+        {{-- Aqui ficarão os cards com os produtos --}}
+    </div>
+    <div class="banner">
+        <img src="{{asset('img/prateleiras.jpg')}}" class="banner-img img-fluid" alt="Banner da Hulmer's com prateleiras">
+        <figcaption><span id="variedade-qual">Variedade</span> e <span id="variedade-qual">qualidade</span> você encontra aqui</figcaption>
+    </div>
+@endsection
---
+++ e-commerce-bala/resources/views/quemsomos.blade.php
@@ -0,0 +1,28 @@
+@extends('layout.main')
+@section('css')
+    <link rel="stylesheet" href="/css/quemsomos.css">
+@endsection
+
+@section('conteudo')
+    
+    <div class="img-fluid banner01"></div>
+    <div class="container mt-3 mb-4">
+        <h1 class="text-center mb-3">Quem somos?</h1>
+            <p>Olá! somos a Hulmer's, uma empresa do ramo da boa cerveja. A hulmer's vem desde 2010 trazendo as cervejas da melhor qualidade para você não se preocupar com nada, apenas em bebê-la.</p>
+
+            <p>A Hulmer's leva no conforto de sua casa, festa ou até Iate a suas bebidas bem geladas e de qualidade.</p>
+    </div>
+    <div class="img-fluid banner02"></div>
+    <div class="container mt-3">
+        <h1 class="text-center mb-3">Onde nos Localizamos?</h1>
+        <p>Nosso espaço aconchegante irá trazer uma ótima experiência para você e seus amigos. Venha nos conhecer!</p>
+        <p>Dê uma passadinha em nosso estabelecimento ou reserve um horário.</p>
+    </div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d230.4386143936069!2d-44.45523285257837!3d-22.46596425337237!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9e7e95e3a41dc5%3A0x5de60073966f5d2d!2sSalesiano!5e0!3m2!1sen!2sbr!4v1625790059225!5m2!1sen!2sbr" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" class="mb-5"></iframe>
+    
+    <div class="container mt-3">
+
+        <p class="text-center">Entre em contato conosco na página de <a href="{{route('contato')}}">contato</a> ou pelo nosso whatsapp (24) 999844243 <i class="fab fa-whatsapp"></i></p>
+    </div>
+
+@endsection
