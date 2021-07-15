d74e89a5f0f0521389612fb6fef3528a5f939f3c
Pedro Leopoldo <pedro.leopoldo@estudante.ufjf.br>
2021-07-15 16:44:10 -0300

    Pagina de quem somos e home

--- e-commerce-bala/resources/views/layout/main.blade.php
+++ e-commerce-bala/resources/views/layout/main.blade.php
@@ -8,12 +8,13 @@
 
     {{-- Css Bootstrap --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    {{-- Font Awesome --}}
+    <script src="https://kit.fontawesome.com/1194f437a6.js" crossorigin="anonymous"></script>
+    @yield('css')
 </head>
 <body>
     
-    <div class="container">
         @yield('conteudo')
-    </div>
 
 </body>
 </html>
--- e-commerce-bala/routes/web.php
+++ e-commerce-bala/routes/web.php
@@ -13,8 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::view('/contato', 'contato');
+Route::view('/', 'home')->name('home');
+Route::view('/contato', 'contato')->name('contato');
+Route::view('/quemsomos', 'quemsomos')->name('quemsomos');
