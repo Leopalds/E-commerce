034abcd681d39bffc91d2d5e6b6b19e5324aef81
jaum1234 <joaovitorsouzacoura@gmail.com>
2021-07-19 07:52:49 -0300

    Iniciando desenvolvimento da pagina individual de cada produto e criando rota do produto individual

---
+++ e-commerce-bala/resources/views/produto/produto-individual.blade.php
@@ -0,0 +1,25 @@
+@extends('layout.main')
+
+@section('conteudo')
+
+    <div>
+        <div>
+            <img src="" alt="...">
+            <img src="" alt="...">
+            <img src="" alt="...">
+        </div>
+        <div>
+            <img src="" alt="...">
+        </div>
+        <div>
+            <div>
+                <h5></h5>
+                <p>Preço</p>
+                <label for="qtd" class="form-label">Qtd</label>
+                <input type="number" name="qtd" class="form-control">
+            </div>
+            <a href="#" class="btn produto_botao-add-carrinho">Adicionar ao carrinho</a>
+        </div>
+    </div>
+
+@endsection
--- e-commerce-bala/routes/web.php
+++ e-commerce-bala/routes/web.php
@@ -6,5 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'home')->name('home');
 
 Route::view('/produtos', 'produto.produto-lista')->name('produtos');
+Route::view('/produtos/individual', 'produto.produto-individual')->name('produtos-individual');
 Route::view('/contato', 'contato')->name('contato');
 Route::view('/quemsomos', 'quemsomos')->name('quemsomos');
