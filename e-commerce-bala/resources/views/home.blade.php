c46744f2001322e25adcdd3c2feef642b8f30612
jaum1234 <joaovitorsouzacoura@gmail.com>
2021-07-27 07:04:11 -0300

    Removendo object-position dos banners

--- e-commerce-bala/resources/views/home.blade.php
+++ e-commerce-bala/resources/views/home.blade.php
@@ -4,27 +4,29 @@
     <link rel="stylesheet" href="css/home.css">
     <link rel="stylesheet" href="css/home/home-banner.css">
     <link rel="stylesheet" href="css/home/home-banner__img.css">
+    <link rel="stylesheet" href="{{ asset('css/botao-carrinho/botao-carrinho.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/produtos/produtos__nome.css') }}">
     
 @endsection
 
 @section('conteudo')
     <div class="home-banner">
-        <img src="{{asset('../img/bebidas.jpg')}}" class="home-banner__img img-fluid" alt="Banner da Hulmer's">
-        <figcaption>A sua cerveja entregue por quem <span>ama</span></figcaption>
+        <img src="{{asset('/img/banner1-photoshop.png')}}" class="home-banner__img img-fluid" alt="Banner da Hulmer's">
     </div>
-    <div class="container d-flex flex-wrap justify-content-center mb-3">
-        <h2 class="text-center mt-3">Produtos que Hulmer's recomenda para você</h2>
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div class="container mb-5 mt-5">
+        <h2 class="text-center mt-3 mb-4">Produtos que Hulmer's recomenda para você</h2>
+        <div class="d-flex flex-wrap justify-content-center mb-3">
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title produtos__nome">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn botao-carrinho">Adicionar ao carrinho</a>
+                </div>
             </div>
-          </div>
+        </div>
     </div>
     <div class="home-banner">
-        <img src="{{asset('img/prateleiras.jpg')}}" class="home-banner__img img-fluid" alt="Banner da Hulmer's com prateleiras">
-        <figcaption><span id="variedade-qual">Variedade</span> e <span id="variedade-qual">qualidade</span> você encontra aqui</figcaption>
+        <img src="{{asset('img/banner2-photoshop.png')}}" class="home-banner__img img-fluid" alt="Banner da Hulmer's com prateleiras">
     </div>
 @endsection
