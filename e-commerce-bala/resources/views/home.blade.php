4854e2a1ec32c30ca4d8963520e3b70be6dc574f
jaum1234 <joaovitorsouzacoura@gmail.com>
2021-07-27 07:33:21 -0300

    Resize dos banners para os viewports 1500px, 900px e 650px

--- e-commerce-bala/resources/views/home.blade.php
+++ e-commerce-bala/resources/views/home.blade.php
@@ -11,7 +11,11 @@
 
 @section('conteudo')
     <div class="home-banner">
-        <img src="{{asset('/img/banner1-photoshop.png')}}" class="home-banner__img img-fluid" alt="Banner da Hulmer's">
+        <picture>
+            <source media="(max-width: 650px)" srcset="{{ asset('img/banner1-mobile.png') }}">
+            <source media="(max-width: 900px)" srcset="{{ asset('img/banner1-tablet.png') }}">
+            <img src="{{asset('/img/banner1-photoshop.png')}}" class="home-banner__img img-fluid" alt="Banner da Hulmer's">
+        </picture>
     </div>
     <div class="container mb-5 mt-5">
         <h2 class="text-center mt-3 mb-4">Produtos que Hulmer's recomenda para você</h2>
@@ -27,6 +31,10 @@
         </div>
     </div>
     <div class="home-banner">
-        <img src="{{asset('img/banner2-photoshop.png')}}" class="home-banner__img img-fluid" alt="Banner da Hulmer's com prateleiras">
+        <picture>
+            <source media="(max-width: 650px)" srcset="{{ asset('img/banner2-mobile.png') }}">
+            <source media="(max-width: 900px)" srcset="{{ asset('img/banner2-tablet.png') }}">
+            <img src="{{asset('img/banner2-photoshop.png')}}" class="home-banner__img img-fluid" alt="Banner da Hulmer's com prateleiras">
+        </picture>
     </div>
 @endsection
