c605096e8fd9677cbb115ff2b9d63b330c6f96d2
jaum1234 <joaovitorsouzacoura@gmail.com>
2021-09-30 14:35:25 -0300

    template introduzida

--- e-commerce-bala/resources/views/admin/admin.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('adminlte::page')
-
-@section('content_header')
-    <h1>Dashboard</h1>
-@stop
--- e-commerce-bala/routes/web.php
+++ e-commerce-bala/routes/web.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::view('/', 'home')->name('home');
+Route::view('/', 'pages.home')->name('home');
 
 Route::view('/produtos', 'pages.produto.produto-lista')->name('produtos');
 Route::view('/produtos/id', 'pages.produto.produto-individual')->name('produtos-individual');
 Route::view('/contato', 'pages.contato')->name('contato');
 Route::view('/quemsomos', 'pages.quemsomos')->name('quemsomos');
+
+Route::view('/admin', 'admin.dashboard');
