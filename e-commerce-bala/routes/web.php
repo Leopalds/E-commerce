0be8dd77efd2d94914417cab967fdfaa4ae48b23
jaum1234 <joaovitorsouzacoura@gmail.com>
2021-10-05 07:58:22 -0300

    npm

--- e-commerce-bala/routes/web.php
+++ e-commerce-bala/routes/web.php
@@ -1,61 +1,24 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Redirect;
-use App\Http\Controllers\ProdutoController;
-use App\Http\Controllers\CategoriaController;
 
-Route::redirect('/home', '/', 301);
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-Route::view('/', 'pages.home')->name('home');
-
-Route::view('/produtos', 'pages.produto.produto-lista')->name('produtos');
-Route::view('/produtos/{id}', 'pages.produto.produto-individual')->name('produtos-individual');
-Route::view('/contato', 'pages.contato')->name('contato');
-Route::view('/quemsomos', 'pages.quemsomos')->name('quemsomos');
-
-Route::prefix('/admin')->group(function () {
-    
-    Route::view('', 'admin.dashboard');
-
-    Route::resource('produtos', ProdutoController::class)
-        ->names([
-        'index' => 'admin.produtos.index',
-        'show' => 'admin.produtos.show',
-        'create' => 'admin.produtos.create',
-        'store' => 'admin.produtos.store',
-        'edit' => 'admin.produtos.edit',
-        'update' => 'admin.produtos.update',
-        'destroy' => 'admin.produtos.destroy'
-        ]
-    )->parameters([
-        'produtos' => 'id'
-    ]);
-
-    Route::resource('categorias', CategoriaController::class)
-        ->except([
-            'show'
-        ]
-    )->names([
-        'index' => 'admin.categorias.index',
-        'create' => 'admin.categorias.create',
-        'store' => 'admin.categorias.store',
-        'edit' => 'admin.categorias.edit',
-        'update' => 'admin.categorias.update',
-        'destroy' => 'admin.categorias.destroy'
-        ]
-    )->parameters([
-        'categorias' => 'id'
-        ]
-    );
+Route::get('/', function () {
+    return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Auth::routes(
-    ['register' => false],
-);
-Route::redirect('/register', '/login', 301);
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+require __DIR__.'/auth.php';
