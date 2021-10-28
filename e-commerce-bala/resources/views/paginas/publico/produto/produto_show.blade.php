556ef40000a2856509565c185ea18ce7be7ac82d
jaum1234 <joaovitorsouzacoura@gmail.com>
2021-10-28 11:21:38 -0300

    consertando responsividade da pagina de produto

--- e-commerce-bala/resources/views/paginas/publico/produto/produto_show.blade.php
+++ e-commerce-bala/resources/views/paginas/publico/produto/produto_show.blade.php
@@ -30,37 +30,39 @@
             <p class="mb-4">{{ $produto->descricao }}</p>
             <p class="mb-4 fw-bold fs-3">R$ {{ number_format($produto->preco, 2, ',', '.') }}</p> 
             <hr>
-            <form action="{{ route('frete.store') }}" method="POST">
-                @csrf
-                <div class="mb-3">
-                    <div class="d-flex align-items-center mb-2">
-                        <label class="form-label me-2">Calcular o frete:</label>
-                        <input name="cep" type="text" class="form-control w-25" placeholder="99.999-999">
-                        <input name="quantidade_frete" id="quantidade-frete" type="number" hidden>
-                    </div>
-                    @if (Session::has('frete'))
-                        <div>
-                            <small class="text-success">
-                                Valor do frete: R$ {{ number_format(Session::get('frete'), 2, ',', '.') }}
-                            </small>
-                        </div>
-                    @endif
-                    <button class="btn btn-danger">Calcular</button>
-                </div>
-            </form>
-            <div class="quantidade mb-5">
-                <form action="{{ route('carrinho.store') }}" method="POST" class="d-flex flex-column align-items-start">
+            <div class="produto__calculo w-100">
+                <form action="{{ route('frete.store') }}" method="POST" >
                     @csrf
-                    <div class="d-flex flex-column">
-                        <input type="hidden" name="produto_id" value="{{ $produto->id }}">
-                        <label for="qtd" class="form-label me-3 quantidade__rotulo">Quantidade</label>
-                        <input type="number" value="1" min="0" id="qtd" name="quantidade" class="form-control px-2 quantidade__campo w-50">
+                    <div class="mb-3">
+                        <div class="d-flex align-items-center mb-2">
+                            <label class="form-label me-2">Calcular o frete:</label>
+                            <input name="cep" type="text" class="form-control w-25" placeholder="99.999-999">
+                            <input name="quantidade_frete" id="quantidade-frete" type="number" hidden>
+                        </div>
+                        @if (Session::has('frete'))
+                            <div>
+                                <small class="text-success">
+                                    Valor do frete: R$ {{ number_format(Session::get('frete'), 2, ',', '.') }}
+                                </small>
+                            </div>
+                        @endif
+                        <button class="btn btn-danger">Calcular</button>
                     </div>
-                    @livewire('botao.botao', [
-                        'conteudo' => 'Adicionar ao carrinho',
-                        'extraCss' => 'botao botao--carrinho mt-3'
-                    ])
                 </form>
+                <div class="quantidade mb-5">
+                    <form action="{{ route('carrinho.store') }}" method="POST" class="d-flex flex-column align-items-start quantidade_form">
+                        @csrf
+                        <div class="d-flex flex-column">
+                            <input type="hidden" name="produto_id" value="{{ $produto->id }}">
+                            <label for="qtd" class="form-label me-3 quantidade__rotulo">Quantidade</label>
+                            <input type="number" value="1" min="0" id="qtd" name="quantidade" class="form-control px-2 quantidade__campo w-50">
+                        </div>
+                        @livewire('botao.botao', [
+                            'conteudo' => 'Adicionar ao carrinho',
+                            'extraCss' => 'botao botao--carrinho mt-3'
+                        ])
+                    </form>
+                </div>
             </div>
             <div>Estoque: {{ $produto->quantidade }}</div>
         </div>
